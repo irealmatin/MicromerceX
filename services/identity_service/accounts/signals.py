704b9614ab5d10635ed3ca4638e783ffe672dbd6
matin <irealmatin@gmail.com>
2025-07-12 12:38:00 +0330

    feat:add user identity functionality

--- services/identity_service/accounts/signals.py
+++ services/identity_service/accounts/signals.py
@@ -4,6 +4,9 @@ from .models import User, UserProfile
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    automaticly create a profile user when new user register .
+    """
     if created:
         UserProfile.objects.create(user=instance)
         print(f"Profile created for user {instance.username}")
